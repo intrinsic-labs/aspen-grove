9a32b5a0302cd54e9b9b876f265b5cc92853c27e
rocketbro <asherpope@gmail.com>
2026-02-04 11:43:21 -0500

    add path aliases

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+            '@domain': './src/domain',
+            '@application': './src/application',
+            '@infrastructure': './src/infrastructure',
+            '@interface': './src/interface',
+          },
+        },
+      ],
+    ],
   };
 };
